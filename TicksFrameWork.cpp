615797e2a54f239bb9a4907f6c919a1974c1f9a7
Venkatramayya Yalamati <152295993+venkatyalamati@users.noreply.github.com>
2025-03-23 19:33:26 +0530

    Update TicksFrameWork.cpp

--- TicksFrameWork.cpp
+++ TicksFrameWork.cpp
@@ -1,3 +1,18 @@
+using namespace std;
 class Tiks
 {
-}
+  private:
+    int _cnt, _cntMax;
+  public:
+    Tiks(int cntMax){
+      _cntMax = cntMax;
+      _cnt = 0;
+    }
+    void incTik(){
+      _cnt++;
+      if(_cnt > _cntMax){
+        _cnt = 0;
+        taskTik();
+      }
+    }
+};
